cab280d82108c46b964878ae316978b4f60dc46d
saraoswald <sara.linsley@gmail.com>
2020-02-18 10:03:24 -0500

    refactor parabolas

--- Rainbow Layer Colors.js
+++ Rainbow Layer Colors.js
@@ -9,22 +9,26 @@ function getRGB(i, len) {
     return [red(place) * 250, green(place) * 250, blue(place) * 250];
 }
 
+function curve(x, xOffset) {
+    return -16 * Math.pow(x - xOffset, 2) + 1
+}
+
 function red(x) {
     if (x < 0.25) return 1;
-    if (x >= 0.25 && x <= 0.5) return -16 * Math.pow(x - 0.25, 2) + 1;
-    if (x >= 0.75) return -16 * Math.pow(x - 1, 2) + 1;
+    if (x >= 0.25 && x <= 0.5) return curve(x, 0.25);
+    if (x >= 0.75) return curve(x, 1);
     return 0;
 }
 
 function green(x) {
-    if (x <= 0.25) return (-16 * Math.pow(x - 0.25, 2)) + 1;
+    if (x <= 0.25) return curve(x, 0.25);
     if (x >= 0.25 && x <= 0.5) return 1;
-    if (x >= 0.5 && x <= 0.75) return (-16 * Math.pow(x - 0.5, 2)) + 1;
+    if (x >= 0.5 && x <= 0.75) return curve(x, 0.5);
     return 0;
 }
 
 function blue(x) {
-    if (x >= 0.5 && x <= 0.75) return (-16 * Math.pow(x - 0.75, 2)) + 1;
+    if (x >= 0.5 && x <= 0.75) return curve(x, 0.75);
     if (x > 0.75) return 1;
     return 0;
 }
