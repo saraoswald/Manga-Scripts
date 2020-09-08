7771cdf058147f827bd0008fa7750ee81c79f17f
saraoswald <sara.linsley@gmail.com>
2020-09-08 14:43:23 -0400

    Create Go to Page.js

---
+++ Go to Page.js
@@ -0,0 +1,62 @@
+/* Go to Page.js
+
+    Works like the built-in "Go to Page" function, but also works for "backwards" (Left to Right) books. 
+    Uses the pageBinding setting in the document to determine which InDesign page to take you to. 
+    
+    For example,
+    | Input | Page Binding Setting | Final Page |
+    |-------|----------------------|------------|
+    |   40  |     Left to Right    |    160     |
+    |   40  |     Right to Left    |     40     |
+
+
+    Installation Instructions: https://github.com/saraoswald/Manga-Scripts/#how-to-use-scripts-in-indesign
+    
+*/
+
+
+var isLtR = app.documents[0].documentPreferences.pageBinding == PageBindingOptions.LEFT_TO_RIGHT;
+var doc = app.activeDocument;
+
+function myDisplayDialog() {
+    myDialog = app.dialogs.add({ name: "Go to Page" });
+    var selectedPageNumber;
+    var bookSize = parseInt(doc.pages.lastItem().name);
+    with(myDialog) {
+        with(dialogColumns.add()) {
+            var currentPageNum = parseInt(app.activeWindow.activePage.name);
+            var bookPageNum = isLtR ? bookSize - currentPageNum + 1 : currentPageNum;
+            selectedPageNumber = integerEditboxes.add({
+                editValue: bookPageNum,
+                minimumValue: parseInt(doc.pages.firstItem().name),
+                maximumValue: bookSize
+            });
+        }
+    }
+    var myReturn = myDialog.show();
+    if (myReturn == true) {
+        var needsReview = false;
+
+        if (selectedPageNumber.editValue < 1) {
+            alert('Please enter a valid page number');
+            needsReview = true;
+        }
+
+        if (!needsReview) {
+            var newPageNum = isLtR ? bookSize - selectedPageNumber.editValue + 1 : selectedPageNumber.editValue;
+            var newPage = doc.pages.itemByName(newPageNum.toString());
+
+            app.activeWindow.activePage = newPage.isValid ?
+                newPage :
+                (isLtR ? doc.pages.firstItem() : doc.pages.lastItem());
+        }
+
+        myDialog.destroy();
+    } else {
+        myDialog.destroy();
+    }
+}
+
+try {
+    myDisplayDialog();
+} catch (err) { alert(err) }
