138b127e51f37a4e43e71bbe6c36f5d6579529b0
saraoswald <sara.linsley@gmail.com>
2020-09-25 18:05:45 -0400

    better error handling, refactoring

--- Place Art.js
+++ Place Art.js
@@ -1,20 +1,29 @@
-/* Version 1.0
-
-    documentation coming soon sorrrrryyyyyyyy
+/* 
+    Place Art.js
+    
+    ----------------
 
     Installation instructions here: https://github.com/saraoswald/Manga-Scripts#how-to-use-scripts-in-indesign
+
+    Usage Instructions: 
+        - Create a new document with the correct size specifications.
+        - Run Place Art.js.
+        - Select your art files from the selection menu.
+        - Wait for the art to be placed. You should see images appearing on the pages and links appearing in the Links Panel. If not, you might need to place the art manually.
+
+    Important Notes: 
+        - This is a really intensive operation on your computer. You might need to do it in batches. 
+        - This script *will not* place art on locked layers, so protect your other work by locking layers 
+        - This was originally intended for manga placement, which is "backwards" compared to other English-language books.
+          Change the "thisIsManga" variable just below here if you'd like to use this for non-manga projects. 
 */
+
+var thisIsManga = true; // change this to "false" (no quotation marks) if you don't need art to be placed backwards
 
 var doc = app.activeDocument;
 var bookSize = doc.pages.count();
 var isLtR = doc.documentPreferences.pageBinding == PageBindingOptions.LEFT_TO_RIGHT;
 
-var extractPageNum = function(path) {
-    var regex = /\d{3,4}(?=\_?\d?\.[A-Za-z]{3,4})/;
-    var regexResult = regex.exec(path);
-    return path && regexResult !== null && regexResult.length > 0 ?
-        parseInt(regexResult[0], 10) : 0;
-}
 
 var targetLayer = doc.layers.itemByName('Art').isValid ?
     doc.layers.itemByName('Art') :
@@ -31,17 +40,58 @@ function getFilter() {
 try {
     var artFiles = File.openDialog('Select art files to place', getFilter(), true);
     if (artFiles !== null && artFiles.length > 0) { // in case the user pressed cancel or something
-        for (var i = 0; i < artFiles.length; i++) {
-            var pageNum = extractPageNum(artFiles[i]);
-            if (pageNum > 0) {
-                var image = new File(artFiles[i]);
-                var bookPageNum = isLtR ? bookSize - pageNum + 1 : pageNum;
-                var page = doc.pages.itemByName(bookPageNum.toString());
-                placeArtOnPage(page, targetLayer, image);
-            }
+        var pagesCount = 0; // for debugging :')
+        var hasErrors = false;
+        for (var i = 0; !hasErrors && i < artFiles.length; i++) {
+            hasErrors = placeArtOnPage(artFiles[i]);
+            if (!hasErrors) pagesCount++;
         }
+        if (!hasErrors && pagesCount === 0) alert("No art files found! Make sure the file names are formatted like 123.TIF");
     }
 } catch (err) { alert(err) }; // the debugger that will never let u down :')
+
+
+function placeArtOnPage(artLink) {
+    var pageNum = extractPageNum(artLink)
+    var hasErrors = pageNum < 1;
+    if (!hasErrors) {
+        var image = new File(artLink);
+        // determine INDD page number based on book binding
+        var bookPageNum = thisIsManga && isLtR ? bookSize - pageNum + 1 : pageNum; // LtR books are "backwards", where "Page 1" the last page in the document
+        var page = doc.pages.itemByName(bookPageNum.toString()); // which page in the INDD to place the art on
+
+        hasErrors = prePlaceErrorHandling(page, bookPageNum, bookSize, image, artLink);
+
+        if (!hasErrors) {
+            placeArtOnPageHelper(page, targetLayer, image);
+        }
+    }
+    return hasErrors;
+}
+
+function placeArtOnPageHelper(page, layer, image) {
+    var frame = getFrame(page, layer);
+    if (frame) frame.place(image);
+}
+
+function prePlaceErrorHandling(page, pageNum, image, imageLink) {
+    var debugInfo = "\n\n----Debug Info Below----\nPage number the script calculated from the link: " + pageNum.toString() + "\nNumber of pages in book: " + bookSize.toString() + "\nImage that messed up: " + imageLink;
+
+    if (pageNum < 0 || pageNum > bookSize) {
+        alert("Not enough pages in this document.\nAdd some more pages, and try again.\nIf that doesn't work, try renaming your files to look like '123.TIF'." + debugInfo);
+        return true;
+    }
+    if (!page.isValid || pageNum === 0) {
+        alert("Failed to match the number in the file name to one in the book.\nTry renaming your files to look like '123.TIF'." + debugInfo);
+        return true;
+    }
+    if (image.error) {
+        alert("Something wrong with the Image\nReview the image link below. Not sure when this would happen, but InDesign couldn't create a File out of the image's link" + debugInfo)
+        return true;
+    }
+
+    return false; // yay no errors!!
+}
 
 function getFrame(page, layer) {
     var targetFrame = null;
@@ -50,9 +100,10 @@ function getFrame(page, layer) {
         var returnVal = null;
         // iterate backwards because it's most likely on the bottom
         for (var i = arr.length; i > -1; i-- && returnVal === null) {
+            // only grab an object if it's a Rectangle on a non-locked layer
             if (arr[i] && arr[i] instanceof Rectangle &&
                 !arr[i].itemLayer.locked &&
-                (ignoreLayer || arr[i].itemLayer == layer)) {
+                (ignoreLayer || arr[i].itemLayer == layer)) { // if using an element from a master, don't be picky about the layer
                 returnVal = arr[i];
             }
         }
@@ -61,7 +112,7 @@ function getFrame(page, layer) {
 
     // check if there's a master, override the master items and use the frame there 
     if (page.appliedMaster !== null && page.appliedMaster.isValid) {
-        var masterFrame = findFrame(page.masterPageItems, true);
+        var masterFrame = findFrame(page.masterPageItems, true); // look for a frame, but don't be picky about which layer it's on
         targetFrame = masterFrame && masterFrame.isValid ?
             masterFrame.override(page) : null;
     };
@@ -77,23 +128,23 @@ function getFrame(page, layer) {
     return targetFrame;
 }
 
-// place art on page function / inputs: page & image link
-function placeArtOnPage(page, layer, image) {
-    if (!page.isValid || !layer.isValid || !image.exists) return;
-
-    var frame = getFrame(page, layer);
-    if (frame) frame.place(image);
+function extractPageNum(path) {
+    var regex = /\d{3,4}(?=\_?\d?\.[A-Za-z]{3,4})/;
+    var regexResult = regex.exec(path);
+    return path && regexResult !== null && regexResult.length > 0 ?
+        parseInt(regexResult[0], 10) : 0;
 }
 
 // takes bounds input and gives output in the form [y1, x1, y2, x2]
+// adds the document setting for bleed, taking into account the gutter
 function getPageBounds(page) {
     var prfs = doc.documentPreferences,
-        pg = page.bounds,
+        pb = page.bounds,
         bleedLeft = page.side == PageSideOptions.RIGHT_HAND ? 0 : prfs.documentBleedInsideOrLeftOffset,
         bleedRight = page.side == PageSideOptions.LEFT_HAND ? 0 : prfs.documentBleedOutsideOrRightOffset;
-    var y1 = pg[0] - prfs.documentBleedTopOffset,
-        x1 = pg[1] - bleedLeft,
-        y2 = pg[2] + prfs.documentBleedBottomOffset,
-        x2 = pg[3] + bleedRight;
+    var y1 = pb[0] - prfs.documentBleedTopOffset,
+        x1 = pb[1] - bleedLeft,
+        y2 = pb[2] + prfs.documentBleedBottomOffset,
+        x2 = pb[3] + bleedRight;
     return [y1, x1, y2, x2];
 }
