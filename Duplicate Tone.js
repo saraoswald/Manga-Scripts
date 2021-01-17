bb428d2228300f025c75ecbfe39447e59bcb3d05
saraoswald <sara.linsley@gmail.com>
2021-01-17 10:58:54 -0500

    Add support for multiple directions

--- Duplicate Tone.js
+++ Duplicate Tone.js
@@ -1,13 +1,12 @@
 /*
  ----------- Duplicate Tone -----------  
 
-    Duplicates the selection a given number of times to the right. 
+    Duplicates the selection a given number of times in a given direction. 
+    Perfect for reconstructing tone from a small sample.
 
     Tested in: Photoshop CC 2020
 
-    Updated: Jan 16 2021
-
-    Note: For now this script only duplicates tone directly to the right
+    Updated: Jan 17 2021
 
     Usage: 
         - Select a pattern-able section of halftone
@@ -16,26 +15,93 @@
 */
 
 
-function main() {
+/* ----- Constants ----- */
 
-    var iterations = prompt("How many times should the selection be duplicated?", 5, "Tone Duplicator");
-    if (!iterations) return;
+var DIRECTIONS = [
+    "Right",
+    "Left",
+    "Up",
+    "Down"
+];
 
+/* ----- Functions ----- */
+
+function main(iterationCount, direction) {
     // make a selection 
     var selection = app.activeDocument.selection;
-    var bounds = getBounds(selection);
+    var deltaValues = getDeltaValues(direction, selection);
 
-    // var layerSet = app.activeDocument.layerSets.add();
     var target = null;
-    var layers = [];
-    for (var i = 0; i < iterations; i++) {
-        target = duplicateAndMove(target || selection, bounds.width, 0);
+    var layers = []; // keep track of new layers (using the LayerSet class gave me lots of issues, including layer reordering and unexplained errors on merge)
+    for (var i = 0; i < iterationCount; i++) {
+        target = duplicateAndMove(target || selection, deltaValues);
         layers.push(target);
     }
 
-    mergeLayers(layers);
+    mergeLayers(layers); // merge all the new layers into one
 }
 
+// returns iteration count and duplication direction
+function doPrompt() {
+    if (getErrors()) return;
+
+    var myDialog = new Window("dialog", "Tone Duplicator", undefined);
+
+    var inputGroup = myDialog.add("group");
+    inputGroup.alignChildren = "left";
+    inputGroup.orientation = "column";
+
+    var iterationGroup = inputGroup.add("group");
+    iterationGroup.add("statictext", undefined, "Number of times to duplicate the selection:");
+    var iterationCount = iterationGroup.add("edittext", undefined, "5");
+
+    var directionGroup = inputGroup.add("group");
+    directionGroup.add("statictext", undefined, "Duplication direction:");
+    var directionDD = directionGroup.add("dropdownlist", undefined, DIRECTIONS);
+    directionDD.selection = 0;
+
+    var buttonGroup = myDialog.add("group");
+    buttonGroup.alignment = "right";
+    buttonGroup.add("button", undefined, "OK");
+    buttonGroup.add("button", undefined, "Cancel");
+
+
+    var myReturn = myDialog.show();
+    if (myReturn == true) {
+        var parsedIterationCount = parseInt(iterationCount.text);
+
+        if (parsedIterationCount !== NaN) {
+            main(parsedIterationCount, directionDD.selection.toString());
+        }
+    } else {
+        myDialog.close();
+    }
+}
+
+function getDeltaValues(direction, selection) {
+    var bounds = getBounds(selection),
+        deltaX = 0,
+        deltaY = 0;
+
+    if (direction === "Right") {
+        deltaX = bounds.width;
+    } else if (direction === "Left") {
+        deltaX = bounds.width * -1;
+    } else if (direction === "Up") {
+        deltaY = bounds.height * -1;
+    } else if (direction === "Down") {
+        deltaY = bounds.height;
+    }
+
+    return {
+        deltaX: deltaX,
+        deltaY: deltaY
+    }
+
+}
+
+// converts an array of coordinates into an object in the format {width, height}
+// makes math easier to understand :')
 function getBounds(area) {
     // bounds = x1, y1, x2, y2
     return {
@@ -46,25 +112,49 @@ function getBounds(area) {
 
 
 // takes a source object, duplicates it, and moves it according to deltaX and deltaY
-function duplicateAndMove(source, deltaX, deltaY) {
+function duplicateAndMove(source, deltaValues) {
     var duplicateLayer = null;
+    // the first iteration will be of type "Selection", so we want to create a new layer out of it by copying and pasting
     if (source instanceof Selection) {
         source.copy();
         duplicateLayer = app.activeDocument.paste();
-    } else if (source instanceof ArtLayer) {
+        duplicateLayer.name = "Tone";
+        // just leave this where it is and only move subsequent iterations
+    } else if (source instanceof ArtLayer) { // subsequent iterations will be their own layers, so we can just duplicate them
         duplicateLayer = source.duplicate();
+        // then move the new layer by given X and Y values
+        duplicateLayer.translate(deltaValues.deltaX, deltaValues.deltaY);
     }
-
-    duplicateLayer.translate(deltaX, deltaY);
     return duplicateLayer;
 }
 
+// given an array of layers, merges them all down into one
 function mergeLayers(layersArr) {
+    // start at 1 instead of zero to skip the oldest layer
     for (var i = 1; i < layersArr.length; i++) {
         layersArr[i].merge();
     }
 }
 
+function getErrors() {
+    var hasErrors = false;
+
+    try {
+        if (!app.activeDocument.selection || !app.activeDocument.selection.bounds) {
+            alert('Please make a selection and try again.');
+            hasErrors = true;
+        } else if (app.activeDocument.mode === DocumentMode.BITMAP) {
+            alert("This script doesn't work on Bitmap images. Please convert to Grayscale and try again.");
+            hasErrors = true;
+        }
+    } catch (err) {
+        alert('Please make a selection and try again.');
+        hasErrors = true;
+    }
+
+    return hasErrors;
+}
+
 try {
-    main();
-} catch (err) { alert(err) } // nature's debugger :v)
+    doPrompt();
+} catch (err) { alert(err) } // nature's debugger :^)
